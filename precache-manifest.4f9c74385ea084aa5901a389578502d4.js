99482833d732d79cd0efcbe0d04884b1e7fd0212
Code Sandbox <hello@codesandbox.io>
2022-05-30 15:09:58 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4f9c74385ea084aa5901a389578502d4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7de4d42abdda33c678fc872683970f3",
+    "url": "https://deeman90.github.io/csb-pdzfd1/index.html"
+  },
+  {
+    "revision": "8a65de38452658e4be6d",
+    "url": "https://deeman90.github.io/csb-pdzfd1/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "69e8e97239f1653bb759",
+    "url": "https://deeman90.github.io/csb-pdzfd1/static/css/main.f03c56c2.chunk.css"
+  },
+  {
+    "revision": "8a65de38452658e4be6d",
+    "url": "https://deeman90.github.io/csb-pdzfd1/static/js/2.c16c6fba.chunk.js"
+  },
+  {
+    "revision": "69e8e97239f1653bb759",
+    "url": "https://deeman90.github.io/csb-pdzfd1/static/js/main.c307fd2f.chunk.js"
+  },
+  {
+    "revision": "cf9c53590a68dee9b615",
+    "url": "https://deeman90.github.io/csb-pdzfd1/static/js/runtime-main.c3e950df.js"
+  },
+  {
+    "revision": "7709c10e8c30753537d94a6b69d7542a",
+    "url": "https://deeman90.github.io/csb-pdzfd1/static/media/aboutBcg.7709c10e.jpeg"
+  },
+  {
+    "revision": "945afb997e0731b038ed58fadacfeb3f",
+    "url": "https://deeman90.github.io/csb-pdzfd1/static/media/contactBcg.945afb99.jpeg"
+  },
+  {
+    "revision": "0819a81634391f8518fe325378b8f994",
+    "url": "https://deeman90.github.io/csb-pdzfd1/static/media/defaultBcg.0819a816.jpeg"
+  },
+  {
+    "revision": "6f19757fb5101d8815a78371b7627756",
+    "url": "https://deeman90.github.io/csb-pdzfd1/static/media/logo.6f19757f.svg"
+  },
+  {
+    "revision": "984874d19d1cdef8dcee5fcea55c082d",
+    "url": "https://deeman90.github.io/csb-pdzfd1/static/media/mainBcg.984874d1.jpeg"
+  },
+  {
+    "revision": "160fddff3ca71887ebf8da3929244379",
+    "url": "https://deeman90.github.io/csb-pdzfd1/static/media/productsBcg.160fddff.jpeg"
+  },
+  {
+    "revision": "9675841294261b773267d557f531a8ff",
+    "url": "https://deeman90.github.io/csb-pdzfd1/static/media/singleProductBcg.96758412.jpeg"
+  },
+  {
+    "revision": "46143c2f25bc4667fac18f5924288217",
+    "url": "https://deeman90.github.io/csb-pdzfd1/static/media/storeBcg.46143c2f.jpeg"
+  }
+]);
